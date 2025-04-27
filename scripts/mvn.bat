ff6a7d478201d21c2b408808f701307d0e13e93a
sclosaf <sclosaf@gmail.com>
2025-04-27 15:57:44 +0200

    Update mvn.bat

--- scripts/mvn.bat
+++ scripts/mvn.bat
@@ -2,8 +2,8 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 if "%~1"=="" (
-    echo Usage: %~nx0 [compile^|test^|package^|javadoc^|clean]
-    exit /b 1
+	echo Usage: %~nx0 [compile^|test^|package^|javadoc^|clean]
+	exit /b 1
 )
 
 call :execute_maven %~1
@@ -13,20 +13,20 @@ set COMMAND=%~1
 
 cd %~dp0..
 
-:: Esegue il comando Maven appropriato
 if /i "%COMMAND%"=="compile" (
-    mvn -s settings.xml compile
+	mvn -s settings.xml compile
 ) else if /i "%COMMAND%"=="test" (
-    mvn -s settings.xml test
+	mvn -s settings.xml test
 ) else if /i "%COMMAND%"=="package" (
-    mvn -s settings.xml package
+	mvn -s settings.xml package
 ) else if /i "%COMMAND%"=="javadoc" (
-    mvn -s settings.xml javadoc:javadoc
+	mvn -s settings.xml javadoc:javadoc
 ) else if /i "%COMMAND%"=="clean" (
-    mvn -s settings.xml clean
+	mvn -s settings.xml clean
 ) else (
-    echo Invalid option: %COMMAND%
-    echo Available options: compile, test, package, javadoc, clean
-    exit /b 1
+	echo Invalid option: %COMMAND%
+	echo Available options: compile, test, package, javadoc, clean
+	exit /b 1
 )
+
 goto :eof
