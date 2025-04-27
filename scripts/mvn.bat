e76e23e72a479dfd55f1b4e62c98cf804e4dab27
sclosaf <sclosaf@gmail.com>
2025-04-27 15:26:39 +0200

    Correction batch file

--- scripts/mvn.bat
+++ scripts/mvn.bat
@@ -1,45 +1,32 @@
 @echo off
-SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-if "%~1"==""
-(
-	echo Usage: %~nx0 [compile^|test^|package^|javadoc^|clean]
-	exit /b 1
+if "%~1"=="" (
+    echo Usage: %~nx0 [compile^|test^|package^|javadoc^|clean]
+    exit /b 1
 )
 
 call :execute_maven %~1
-exit /b %ERRORLEVEL%
 
 :execute_maven
-setlocal
-	set COMMAND=%~1
-	cd %~dp0..
+set COMMAND=%~1
 
-	if "%COMMAND%"=="compile"
-	(
-		mvn -s settings.xml compile
-	)
-	else if "%COMMAND%"=="test"
-	(
-		mvn -s settings.xml test
-	)
-	else if "%COMMAND%"=="package"
-	(
-		mvn -s settings.xml package
-	)
-	else if "%COMMAND%"=="javadoc"
-	(
-		mvn -s settings.xml javadoc:javadoc
-	)
-	else if "%COMMAND%"=="clean"
-	(
-		mvn -s settings.xml clean
-	)
-	else
-	(
-		echo Invalid option: %COMMAND%
-		echo Available options: compile, test, package, javadoc, clean
-		exit /b 1
-	)
-endlocal
+cd %~dp0..
+
+:: Esegue il comando Maven appropriato
+if /i "%COMMAND%"=="compile" (
+    mvn -s settings.xml compile
+) else if /i "%COMMAND%"=="test" (
+    mvn -s settings.xml test
+) else if /i "%COMMAND%"=="package" (
+    mvn -s settings.xml package
+) else if /i "%COMMAND%"=="javadoc" (
+    mvn -s settings.xml javadoc:javadoc
+) else if /i "%COMMAND%"=="clean" (
+    mvn -s settings.xml clean
+) else (
+    echo Invalid option: %COMMAND%
+    echo Available options: compile, test, package, javadoc, clean
+    exit /b 1
+)
 goto :eof
