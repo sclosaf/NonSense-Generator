99f9853cbf7b115ab5c8d04f73498971f5a13197
sclosaf <sclosaf@gmail.com>
2025-04-27 12:43:47 +0200

    First file configuration and scripts

---
+++ scripts/mvn.bat
@@ -0,0 +1,41 @@
+@echo off
+SETLOCAL
+
+if "%~1"==""
+(
+	echo Usage: %~nx0 [compile^|test^|package^|javadoc]
+	exit /b 1
+)
+
+call :execute_maven %~1
+exit /b %ERRORLEVEL%
+
+:execute_maven
+setlocal
+	set COMMAND=%~1
+	cd %~dp0..
+
+	if "%COMMAND%"=="compile"
+	(
+		mvn -s settings.xml compile
+	)
+	else if "%COMMAND%"=="test"
+	(
+		mvn -s settings.xml test
+	)
+	else if "%COMMAND%"=="package"
+	(
+		mvn -s settings.xml package
+	)
+	else if "%COMMAND%"=="javadoc"
+	(
+		mvn -s settings.xml javadoc:javadoc
+	)
+	else
+	(
+		echo Invalid option: %COMMAND%
+		echo Available options: compile, test, package, javadoc
+		exit /b 1
+	)
+endlocal
+goto :eof
---
+++ scripts/mvn.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+execute_maven()
+{
+		cd "$(dirname "$0")/.."
+
+	case $1 in
+		"compile")
+			mvn -s settings.xml compile
+			;;
+
+		"test")
+			mvn -s settings.xml test
+			;;
+
+		"package")
+			mvn -s settings.xml package
+			;;
+
+		"javadoc")
+			mvn -s settings.xml javadoc:javadoc
+			;;
+
+		*)
+			echo "Invalid option: $1"
+			echo "Available options: compile, test, package, javadoc"
+			exit 1
+			;;
+	esac
+}
+
+main()
+{
+	if [ $# -ne 1 ]; then
+		echo "Usage: $0 [compile|test|package|javadoc]"
+		exit 1
+	fi
+
+	execute_maven "$1"
+}
+
+main "$@"
