a7503cc99d02fa6d7e430e5155e28bb78c4c4c8e
sclosaf <sclosaf@gmail.com>
2025-04-30 20:56:43 +0200

    Upgrade to JsonFileHandler.java

--- src/main/java/it/nonsense/util/JsonFileHandler.java
+++ src/main/java/it/nonsense/util/JsonFileHandler.java
@@ -4,106 +4,138 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.JsonIOException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.FileNotFoundException;
+
 import java.util.List;
 import java.util.ArrayList;
-import java.lang.IndexOutOfBoundsException;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class JsonFileHandler
 {
-	private static JsonFileHandler instance = new JsonFileHandler();
+	private static volatile JsonFileHandler instance = new JsonFileHandler();
+	private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+    private final Gson gson;
 
 	private JsonFileHandler()
-	{}
+	{
+		gson = new GsonBuilder().setPrettyPrinting().create();
+	}
 
 	public static JsonFileHandler getInstance()
 	{
 		return instance;
 	}
 
-	public static void appendItemToJson(String filePath, String key, String str) throws IOException
+	public void appendItemToJson(String filePath, String key, String str) throws IOException, IllegalArgumentException
 	{
-		if(!instance.hasJsonKey(filePath, key))
-			throw new IllegalArgumentException();
+		filePath = ensureJsonExtension(filePath);
+		validateFileExists(filePath);
 
-		JsonObject json;
+		lock.writeLock().lock();
 
-		try(FileReader reader = new FileReader(filePath))
+		try
 		{
-			json = JsonParser.parseReader(reader).getAsJsonObject();
+			JsonObject json = readJsonObject(filePath);
+
+			if(!json.has(key) || !json.get(key).isJsonArray())
+				throw new IllegalArgumentException();
+
+			JsonArray elements = json.getAsJsonArray(key);
+
+			for(JsonElement element : elements)
+				if(element.isJsonPrimitive() && element.getAsString().equals(str))
+					return;
+
+			elements.add(str);
+			writeJsonObject(filePath, json);
 		}
-
-		JsonArray elements = json.getAsJsonArray(key);
-
-		for(JsonElement element : elements)
-			if(element.getAsString().equals(str))
-				return;
-
-		elements.add(str);
-
-		try(FileWriter writer = new FileWriter(filePath))
+		finally
 		{
-			writer.write(json.toString());
+			lock.writeLock().unlock();
 		}
 	}
 
-	public static String readItemFromJson(String filePath, String key, int index) throws IOException
+	public String readItemFromJson(String filePath, String key, int index) throws IOException, IllegalArgumentException, IndexOutOfBoundsException
 	{
-		if(!instance.hasJsonKey(filePath, key))
-			throw new IllegalArgumentException();
+		filePath = ensureJsonExtension(filePath);
+		validateFileExists(filePath);
 
 		if(index < 0)
 			throw new IllegalArgumentException();
 
-		try(FileReader reader = new FileReader(filePath))
+		lock.readLock().lock();
+
+		try
 		{
-			JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+			JsonObject json = readJsonObject(filePath);
+
+			if(!json.has(key) || !json.get(key).isJsonArray())
+				throw new IllegalArgumentException();
+
 			JsonArray elements = json.getAsJsonArray(key);
 
-			for(JsonElement element : elements)
-				if(index-- == 0)
-					return element.getAsString();
-		}
+			if(index >= elements.size())
+				throw new IndexOutOfBoundsException();
 
-		throw new IndexOutOfBoundsException();
+			JsonElement element = elements.get(index);
+
+			if(!element.isJsonPrimitive())
+				throw new IllegalArgumentException();
+
+			return element.getAsString();
+		}
+		finally
+		{
+			lock.readLock().unlock();
+		}
 	}
 
-	public static List<String> readListFromJson(String filePath, String key) throws IOException
+	public List<String> readListFromJson(String filePath, String key) throws IOException, IllegalArgumentException
 	{
-		if(!instance.hasJsonKey(filePath, key))
-			throw new IllegalArgumentException();
+		filePath = ensureJsonExtension(filePath);
+		validateFileExists(filePath);
 
-		try(FileReader reader = new FileReader(filePath))
+		lock.readLock().lock();
+
+		try
 		{
-			JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
-			JsonArray elements = json.getAsJsonArray(key);
+			JsonObject json = readJsonObject(filePath);
 
+			if(!json.has(key) || !json.get(key).isJsonArray())
+				throw new IllegalArgumentException();
+
+			JsonArray elements = json.getAsJsonArray(key);
 			List<String> result = new ArrayList<String>();
 
 			for (JsonElement element : elements)
-				result.add(element.getAsString());
+				if(element.isJsonPrimitive())
+					result.add(element.getAsString());
 
 			return result;
 		}
+		finally
+		{
+			lock.readLock().unlock();
+		}
 	}
 
-	private static boolean hasJsonKey(String filePath, String key) throws IOException
+	public boolean hasJsonKey(String filePath, String key) throws IOException, IllegalArgumentException
 	{
-		if (!filePath.toLowerCase().endsWith(".json"))
-			filePath += ".json";
+		filePath = ensureJsonExtension(filePath);
+		validateFileExists(filePath);
 
-		if(!(new File(filePath).exists()))
-			throw new IllegalArgumentException();
+		lock.readLock().lock();
 
-		try(FileReader reader = new FileReader(filePath))
+		try
 		{
-			JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+			JsonObject json = readJsonObject(filePath);
 			String[] keys = key.split("\\.");
 
 			for(String k : keys)
@@ -111,10 +143,65 @@ public class JsonFileHandler
 				if(!json.has(k))
 					return false;
 
-				json = json.getAsJsonObject(k);
+				JsonElement element = json.get(k);
+
+				if(element.isJsonObject())
+					json = element.getAsJsonObject();
+				else if(k != keys[keys.length - 1])
+					return false;
 			}
 
 			return true;
+		}
+		finally
+		{
+			lock.readLock().unlock();
+		}
+	}
+
+	public JsonObject getJsonObject(String filePath) throws IOException, IllegalArgumentException
+	{
+		filePath = ensureJsonExtension(filePath);
+		validateFileExists(filePath);
+
+		lock.readLock().lock();
+
+		try
+		{
+			return readJsonObject(filePath);
+		}
+		finally
+		{
+			lock.readLock().unlock();
+		}
+	}
+
+	private void validateFileExists(String filePath) throws IllegalArgumentException
+	{
+		File file = new File(filePath);
+
+		if(!file.exists() || !file.isFile())
+			throw new IllegalArgumentException();
+	}
+
+	private String ensureJsonExtension(String filePath)
+	{
+		return filePath.toLowerCase().endsWith(".json") ? filePath : filePath + ".json";
+	}
+
+	private JsonObject readJsonObject(String filePath) throws IOException
+	{
+		try(FileReader reader = new FileReader(filePath))
+		{
+			return JsonParser.parseReader(reader).getAsJsonObject();
+		}
+	}
+
+	private void writeJsonObject(String filePath, JsonObject json) throws IOException
+	{
+		try(FileWriter writer = new FileWriter(filePath))
+		{
+			gson.toJson(json, writer);
 		}
 	}
 }
