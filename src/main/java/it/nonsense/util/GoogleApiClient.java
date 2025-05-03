e36521a732b595150b4c6fb6a14511bb62a253c2
sclosaf <sclosaf@gmail.com>
2025-05-03 11:05:05 +0200

    Update .gitignore

--- src/main/java/it/nonsense/util/GoogleApiClient.java
+++ src/main/java/it/nonsense/util/GoogleApiClient.java
@@ -1,1 +1,11 @@
 package it.nonsense.util;
+
+class GoogleApiClient
+{
+	private LanguageServiceClient client;
+
+	public GoogleApiClient() throws IOException
+	{
+		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+	}
+}
