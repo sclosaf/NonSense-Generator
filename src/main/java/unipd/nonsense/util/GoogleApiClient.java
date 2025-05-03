acf36337de4b70adca9a023e185771965fb7dc35
sclosaf <sclosaf@gmail.com>
2025-05-03 16:49:58 +0200

    Test

--- src/main/java/unipd/nonsense/util/GoogleApiClient.java
+++ src/main/java/unipd/nonsense/util/GoogleApiClient.java
@@ -1,1 +1,30 @@
 package unipd.nonsense.util;
+
+public class GoogleApiClient
+{
+	private final LanguageServiceClient client;
+
+	public GoogleApiClient() throws IOException
+	{
+		InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream("config.json");
+
+		if(credentialsStream == null)
+			throw new IOException("File config.json non trovato");
+
+		ServiceAccountCredentials credentials;
+        try(InputStreamReader reader = new InputStreamReader(credentialsStream))
+		{
+				credentials = ServiceAccountCredentials.fromStream(credentialsStream);
+		}
+
+		LanguageServiceSettings settings = LanguageServiceSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+
+		this.client = LanguageServiceClient.create(settings);
+	}
+
+	public void close()
+	{
+		if (client != null)
+			client.close();
+	}
+}
