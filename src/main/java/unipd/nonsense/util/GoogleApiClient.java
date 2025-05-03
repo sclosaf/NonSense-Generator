c687f09f427fc9e7f2b5690d2890e7cd45747367
sclosaf <sclosaf@gmail.com>
2025-05-03 16:55:21 +0200

    Setup for Google Api client

--- src/main/java/unipd/nonsense/util/GoogleApiClient.java
+++ src/main/java/unipd/nonsense/util/GoogleApiClient.java
@@ -1,5 +1,16 @@
 package unipd.nonsense.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.LanguageServiceSettings;
+
 public class GoogleApiClient
 {
 	private final LanguageServiceClient client;
