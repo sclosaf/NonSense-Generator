461e285dba62d4fc74b1dad47141fd1292b4d448
sclosaf <sclosaf@gmail.com>
2025-05-03 12:14:47 +0200

    Added execution plugin

--- src/main/java/it/nonsense/util/GoogleApiClient.java
+++ src/main/java/it/nonsense/util/GoogleApiClient.java
@@ -1,11 +1,1 @@
 package it.nonsense.util;
-
-class GoogleApiClient
-{
-	private LanguageServiceClient client;
-
-	public GoogleApiClient() throws IOException
-	{
-		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
-	}
-}
