b2c3e7be9d054edb82064205def825b616a3a0bc
sclosaf <sclosaf@gmail.com>
2025-05-04 14:36:49 +0200

    Correction

--- src/main/java/unipd/nonsense/App.java
+++ src/main/java/unipd/nonsense/App.java
@@ -1,11 +1,7 @@
 package unipd.nonsense;
 
-import unipd.nonsense.util.LoggerManager;
-
 public class App
 {
 	public static void main(String[] args)
-	{
-		LoggerManager log = new LoggerManager();
-	}
+	{}
 }
--- src/main/java/unipd/nonsense/util/LoggerManager.java
+++ src/main/java/unipd/nonsense/util/LoggerManager.java
@@ -1,11 +1,1 @@
 package unipd.nonsense.util;
-
-import org.apache.logging.log4j.*;
-
-public class LoggerManager
-{
-	private static final Logger logger = LogManager.getLogger(LoggerManager.class);
-
-	public LoggerManager()
-	{}
-}
