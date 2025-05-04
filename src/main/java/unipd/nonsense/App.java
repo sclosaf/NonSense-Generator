3089b3c0f804a730b11d1de4bef7d65ceb6b71d4
sclosaf <sclosaf@gmail.com>
2025-05-04 15:00:48 +0200

    Test

--- src/main/java/unipd/nonsense/App.java
+++ src/main/java/unipd/nonsense/App.java
@@ -1,7 +1,11 @@
 package unipd.nonsense;
 
+import unipd.nonsense.util.LoggerManager;
+
 public class App
 {
 	public static void main(String[] args)
-	{}
+	{
+		LoggerManager log = new LoggerManager();
+	}
 }
