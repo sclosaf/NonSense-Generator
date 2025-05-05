a1df6e1082f4c17edef41d326c2410595d9de5a7
sclosaf <sclosaf@gmail.com>
2025-05-05 17:15:46 +0200

    Testing windows configuration

--- scripts/mvn.bat
+++ scripts/mvn.bat
@@ -7,6 +7,7 @@ if "%~1"=="" (
 )
 
 call :execute_maven %~1
+exit /b %ERRORLEVEL%
 
 :execute_maven
 set COMMAND=%~1
@@ -15,16 +16,22 @@ cd %~dp0..
 
 if /i "%COMMAND%"=="compile" (
 	mvn -s config\settings.xml compile
+	exit /b 0
 ) else if /i "%COMMAND%"=="test" (
 	mvn -s config\settings.xml test
+	exit /b 0
 ) else if /i "%COMMAND%"=="package" (
 	mvn -s config\settings.xml package
+	exit /b 0
 ) else if /i "%COMMAND%"=="javadoc" (
 	mvn -s config\settings.xml javadoc:javadoc
+	exit /b 0
 ) else if /i "%COMMAND%"=="clean" (
 	mvn -s config\settings.xml clean
+	exit /b 0
 ) else if /i "%COMMAND%"=="execute" (
 	java -cp "target\nonsense-generator-1.0.jar;target\libs\*" unipd.nonsense.App
+	exit /b 0
 ) else (
 	echo Invalid option: %COMMAND%
 	echo Available options: compile, test, package, javadoc, clean, execute
