28165befe59a4146ae1e04de473a84fcaa300ab6
sclosaf <sclosaf@gmail.com>
2025-05-05 22:43:27 +0200

    Formatting

--- src/main/java/unipd/nonsense/generator/SentenceGenerator.java
+++ src/main/java/unipd/nonsense/generator/SentenceGenerator.java
@@ -1,1 +1,66 @@
 package unipd.nonsense.generator;
+/*
+import unipd.nonsense.model.Noun;
+import unipd.nonsense.model.Verb;
+import unipd.nonsense.model.Adjective;
+import unipd.nonsense.model.Template;
+
+public class SentenceGenerator
+{
+	private templatesPath = "";
+	private nounsPath = "";
+	private verbsPath = "";
+	private adjectivesPath = "";
+
+	private List<> Nouns;
+	private List<> Verbs;
+	private List<> Adjectives;
+	private List<> Templates;
+
+	public enum Tense
+	{
+		PAST, PRESENT, FUTURE;
+	}
+
+	public SentenceGenerator()
+	{}
+
+	public String generateRandomSentence()
+	{}
+
+	public String generateSentenceWithTemplate() // given Template as argument
+	{}
+
+	public String generateSentenceWithInputWords() // given lists of Nouns, Verbs, Adjectives as arguments
+	{}
+
+	public String generateSentenceWithTense(Tense tense)
+	{}
+
+	private void loadResources()
+	{}
+
+	private void setResourcesPaths(String templatesPath, String nounsPath, String verbsPath, String adjectivesPath)
+	{}
+
+	private <T> T getRandomElement(List<T> list)
+	{}
+
+	// Update resources with new words or templates ???
+	//
+	// private void addNewNouns(List<Noun> nouns)
+	// {}
+	//
+	// private void addNewVerbs(List<Verb> verbs)
+	// {}
+	//
+	// private void addNewAdjectives(List<Adjective> adjectives)
+	// {}
+	//
+	// private void addNewTemplates(List<Template> templates)
+	// {}
+
+	// Helpers
+}
+
+*/
--- src/main/java/unipd/nonsense/generator/TemplateGenerator.java
+++ src/main/java/unipd/nonsense/generator/TemplateGenerator.java
@@ -10,52 +10,57 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class TemplateGenerator {
-    private List<Template> templates;  // Changed to store Template objects
-    private Random random;
-    private static final String TEMPLATE_FILE = "templates.json";
-    
-    public TemplateGenerator() throws IOException {
-        this.templates = new ArrayList<>();
-        this.random = new Random();
-        loadTemplates();
-    }
-    
-    private void loadTemplates() throws IOException {
-        try {
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(TEMPLATE_FILE);
-            if (inputStream == null) {
-                throw new IOException("Resource not found: " + TEMPLATE_FILE);
-            }
-            
-            java.nio.file.Path tempFile = java.nio.file.Files.createTempFile("templates", ".json");
-            java.nio.file.Files.copy(inputStream, tempFile, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
-            String filePath = tempFile.toString();
-            
-            JsonFileHandler jsonHandler = JsonFileHandler.getInstance();
-            JsonObject jsonObject = jsonHandler.getJsonObject(filePath);
-            JsonArray templatesArray = jsonObject.getAsJsonArray("templates");
-            
-            // Create Template objects instead of storing raw strings
-            for (var element : templatesArray) {
-                this.templates.add(new Template(element.getAsString()));
-            }
-            
-            java.nio.file.Files.delete(tempFile);
-            System.out.println("Loaded " + this.templates.size() + " templates from " + TEMPLATE_FILE);
-        } catch (IOException e) {
-            System.err.println("Error reading template file: " + e.getMessage());
-            throw e;
-        }
-    }
-    
-    // Returns a random Template object
-    public Template getRandomTemplate() {
-        if (templates.isEmpty()) {
-            throw new IllegalStateException("No templates loaded");
-        }
-        
-        int randomIndex = random.nextInt(templates.size());
-        return templates.get(randomIndex);
-    }
+public class TemplateGenerator
+{
+	private List<Template> templates;  // Changed to store Template objects
+	private Random random;
+	private static final String TEMPLATE_FILE = "templates.json";
+
+	public TemplateGenerator() throws IOException
+	{
+		this.templates = new ArrayList<>();
+		this.random = new Random();
+		loadTemplates();
+	}
+
+	private void loadTemplates() throws IOException
+	{
+		try
+		{
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(TEMPLATE_FILE);
+
+			if(inputStream == null)
+				throw new IOException("Resource not found: " + TEMPLATE_FILE);
+
+			java.nio.file.Path tempFile = java.nio.file.Files.createTempFile("templates", ".json");
+			java.nio.file.Files.copy(inputStream, tempFile, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+			String filePath = tempFile.toString();
+
+			JsonFileHandler jsonHandler = JsonFileHandler.getInstance();
+			JsonObject jsonObject = jsonHandler.getJsonObject(filePath);
+			JsonArray templatesArray = jsonObject.getAsJsonArray("templates");
+
+			// Create Template objects instead of storing raw strings
+			for(var element : templatesArray)
+				this.templates.add(new Template(element.getAsString()));
+
+			java.nio.file.Files.delete(tempFile);
+			System.out.println("Loaded " + this.templates.size() + " templates from " + TEMPLATE_FILE);
+		}
+		catch (IOException e)
+		{
+			System.err.println("Error reading template file: " + e.getMessage());
+			throw e;
+		}
+	}
+
+	// Returns a random Template object
+	public Template getRandomTemplate()
+	{
+		if(templates.isEmpty())
+			throw new IllegalStateException("No templates loaded");
+
+		int randomIndex = random.nextInt(templates.size());
+		return templates.get(randomIndex);
+	}
 }
