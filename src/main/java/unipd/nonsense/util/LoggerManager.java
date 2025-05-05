df198e289dea13ef07b496dd44db62bcf0cd3129
FabsMacina <fabioceretti04@gmail.com>
2025-05-05 18:40:04 +0200

    Slight improvements for the logs

--- src/main/java/unipd/nonsense/util/LoggerManager.java
+++ src/main/java/unipd/nonsense/util/LoggerManager.java
@@ -1,14 +1,23 @@
 package unipd.nonsense.util;
 import org.apache.logging.log4j.*;
 
-public class LoggerManager
-{
+public class LoggerManager{
+
 	private static final Logger logger = LogManager.getLogger(LoggerManager.class);
 
-	public LoggerManager()
-	{
-		logger.debug("messaggio DEBUG");
-		logger.fatal("messaggio FATAL");
-		logger.debug("messaggio DEBUG");
+	public LoggerManager(){
 	}
+
+	public void logTrace(String entry){
+		logger.trace(entry);
+	}
+
+	public void logDebug(String entry){
+		logger.debug(entry);
+	}
+
+	public void logError(String entry){
+		logger.error(entry);
+	}
+
 }
