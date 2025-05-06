672de0d269806a583f25eaf19d76778986df5f0c
sclosaf <sclosaf@gmail.com>
2025-05-06 16:34:06 +0200

    Correction and adding .json files

--- src/main/java/unipd/nonsense/generator/TemplateGenerator.java
+++ src/main/java/unipd/nonsense/generator/TemplateGenerator.java
@@ -2,59 +2,39 @@ package unipd.nonsense.generator;
 
 import unipd.nonsense.util.JsonFileHandler;
 import unipd.nonsense.model.Template;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class TemplateGenerator
 {
-	private List<Template> templates;  // Changed to store Template objects
+	private List<Template> templates;
 	private Random random;
-	private static final String TEMPLATE_FILE = "templates.json";
+
+	JsonFileHandler jsonHandler;
+
+	private static String templatesPath = "templates.json";
+	private static String templatesKey = "templates";
 
 	public TemplateGenerator() throws IOException
 	{
 		this.templates = new ArrayList<>();
 		this.random = new Random();
+
+		jsonHandler = JsonFileHandler.getInstance();
 		loadTemplates();
 	}
 
 	private void loadTemplates() throws IOException
 	{
-		try
-		{
-			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(TEMPLATE_FILE);
+		jsonHandler = JsonFileHandler.getInstance();
+		List<String> jsonList = jsonHandler.readListFromJson(templatesPath, templatesKey);
 
-			if(inputStream == null)
-				throw new IOException("Resource not found: " + TEMPLATE_FILE);
-
-			java.nio.file.Path tempFile = java.nio.file.Files.createTempFile("templates", ".json");
-			java.nio.file.Files.copy(inputStream, tempFile, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
-			String filePath = tempFile.toString();
-
-			JsonFileHandler jsonHandler = JsonFileHandler.getInstance();
-			JsonObject jsonObject = jsonHandler.getJsonObject(filePath);
-			JsonArray templatesArray = jsonObject.getAsJsonArray("templates");
-
-			// Create Template objects instead of storing raw strings
-			for(var element : templatesArray)
-				this.templates.add(new Template(element.getAsString()));
-
-			java.nio.file.Files.delete(tempFile);
-			System.out.println("Loaded " + this.templates.size() + " templates from " + TEMPLATE_FILE);
-		}
-		catch (IOException e)
-		{
-			System.err.println("Error reading template file: " + e.getMessage());
-			throw e;
-		}
+		for(String element : jsonList)
+			this.templates.add(new Template(element));
 	}
 
-	// Returns a random Template object
 	public Template getRandomTemplate()
 	{
 		if(templates.isEmpty())
