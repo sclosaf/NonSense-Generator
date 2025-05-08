927096fb78ad429b36de6ad7291947c199427120
FabsMacina <fabioceretti04@gmail.com>
2025-05-08 14:21:27 +0200

    noun adn adjective classes added

--- src/main/java/unipd/nonsense/model/Adjective.java
+++ src/main/java/unipd/nonsense/model/Adjective.java
@@ -1,1 +1,17 @@
 package unipd.nonsense.model;
+
+public class Adjective{
+	public Adjective(String newAdj){
+		adj = newAdj;
+	}
+
+	public String getAdjective(){
+		return adj;
+	}
+
+	public void setAdjective(String newAdj){
+		adj = newAdj;
+	}
+
+	private String adj;
+}
--- src/main/java/unipd/nonsense/model/Noun.java
+++ src/main/java/unipd/nonsense/model/Noun.java
@@ -1,1 +1,17 @@
 package unipd.nonsense.model;
+
+public class Noun{
+	public Noun(String newNoun){
+		noun = newNoun;
+	}
+
+	public String getNoun(){
+		return noun;
+	}
+
+	public void setNoun(String newNoun){
+		noun = newNoun;
+	}
+
+	private String noun;
+}
