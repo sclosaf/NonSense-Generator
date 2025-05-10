756efd27e98965d4f67856f7d6dceac5486b8757
FabsMacina <fabioceretti04@gmail.com>
2025-05-10 17:51:21 +0200

    Improved LoggerManager and log configuration

--- src/main/java/unipd/nonsense/util/LoggerManager.java
+++ src/main/java/unipd/nonsense/util/LoggerManager.java
@@ -1,11 +1,13 @@
 package unipd.nonsense.util;
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class LoggerManager{
 
-	private static final Logger logger = LogManager.getLogger(LoggerManager.class);
+	private final Logger logger;
 
-	public LoggerManager(){
+	public LoggerManager(Class <?> myClass){
+		this.logger = LogManager.getLogger(myClass);
 	}
 
 	public void logTrace(String entry){
@@ -15,9 +17,25 @@ public class LoggerManager{
 	public void logDebug(String entry){
 		logger.debug(entry);
 	}
+	
+	public void logWarn(String entry){
+		logger.warn(entry);
+	}
+
+	public void logWarn(String entry, Throwable eccept){
+		logger.warn(entry, eccept);
+	}
 
 	public void logError(String entry){
 		logger.error(entry);
+	}
+
+	public void logError(String entry, Throwable eccept){
+		logger.error(entry, eccept);
+	}
+
+	public void logFatal(String entry){
+		logger.fatal(entry);
 	}
 
 }
