3b82ea922b708e3b8d6f9e57bdff667520956e86
FabsMacina <fabioceretti04@gmail.com>
2025-05-16 18:46:30 +0200

    Update TestNoun.java

--- src/test/java/unipd/nonsense/model/TestNoun.java
+++ src/test/java/unipd/nonsense/model/TestNoun.java
@@ -1,1 +1,35 @@
 package unipd.nonsense.model;
+
+import unipd.nonsense.exceptions.InvalidGrammaticalElementException;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import unipd.nonsense.model.Noun;
+import unipd.nonsense.model.Noun.Number;
+
+@DisplayName("Testing Noun")
+public class TestNoun{
+
+	Noun singularNoun = new Noun("singularNounExample", Number.SINGULAR);
+	Noun pluralNoun = new Noun("pluralNounExample", Number.PLURAL);
+	
+	@Test
+	@DisplayName("Create noun with invalid arguements")
+	public void testInvalidAdjectiveCreation(){
+		assertThrows(InvalidGrammaticalElementException.class, () -> 
+			new Noun("", Number.SINGULAR), "Should throw InvalidGrammaticalElementException");
+	}
+
+	@Test
+	@DisplayName("Getting nouns")
+	public void testGetNoun(){
+		assertEquals(singularNoun.getNoun(), "singularNounExample");
+	}
+
+	@Test
+	@DisplayName("Getting numbers")
+	public void testGetNumber(){
+		assertEquals(singularNoun.getNumber(), Number.SINGULAR);
+		assertEquals(pluralNoun.getNumber(), Number.PLURAL);
+	}
+
+}
